d3d9eb30a6d6ce90029f51d63e785df2796ce8f3
Evgeny Savitsky <savickij.e@gmail.com>
2018-09-05 11:38:31 +0300

    100elementsArray

--- Main.java
+++ Main.java
@@ -1,66 +1,39 @@
 package com.company;
 
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.util.HashMap;
-import java.util.Scanner;
-import  java.io.File;
-import java.io.FileNotFoundException;
-import java.util.StringTokenizer;
 
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println(array100());
 
-        try {
-            System.out.println(nuberOfRepeats(getText()));
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-        try {
-            System.out.println("Number of words in file: " + numberOfWords(getText()));
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
     }
 
-    public static String getText () throws FileNotFoundException {
-        String s = "";
-        Scanner in = new Scanner(new File("d:\\1.txt"));
-        while (in.hasNext())
-            s += in.nextLine() + "\r\n";
-        in.close();
-        s = s.replace(",","").replace(".","").replace("\n", "").replace("\r", "").replace("  ", " ").replace("?", "").replace("!", "");
-        return s.toLowerCase();
-    }
-    public static int numberOfWords(String text) {
-        String[] words = text.split(" ");
-        return words.length;
-    }
+    public static String array100 (){
+        int[] arr = new int[100];
+        for (int i = 0; i < arr.length; i++)
+            arr[i] = (int) (Math.random() * 10);
+        //for (int i: arr)
+           // System.out.println(i + " ");
 
-    public static String nuberOfRepeats (String text) {
-        String[] words = text.split(" ");
-        HashMap<String, Integer> d = new HashMap<>();
+        HashMap<Integer, Integer> d = new HashMap<>();
         Integer counter;
-        for (int i = 0; i < words.length; i++) {
-            if (! d.containsKey(words[i])) {
+        for (int i = 0; i < arr.length; i++) {
+            if (!d.containsKey(arr[i])) {
                 counter = 1;
-                d.put(words[i], counter);
+                d.put(arr[i], counter);
             } else {
-                d.put(words[i], d.get(words[i]) + 1);
+                d.put(arr[i], d.get(arr[i]) + 1);
             }
+
         }
-
-
-
-
-        for (String name : d.keySet()) {
+        for (Integer name : d.keySet()) {
             System.out.println(name + ": " + d.get(name));
         }
 
         return "";
-    }
 
+    }
 
 
 }
